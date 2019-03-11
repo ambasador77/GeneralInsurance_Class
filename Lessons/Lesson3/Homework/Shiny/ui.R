cc50cff40b1036a774ea832c750de174a9f57b65
Tekvicka <47779415+Tekvicka@users.noreply.github.com>
2019-03-11 20:54:54 +0100

    Update ui.R

--- Lessons/Lesson3/Homework/Shiny/ui.R
+++ Lessons/Lesson3/Homework/Shiny/ui.R
@@ -1,37 +1,17 @@
+library(dplyr)
+dt_KPI <- read.csv("./Data/lesson2_KPI.csv") %>% filter_all(all_vars(!is.na(.)))
 
+ui <- fluidPage(
+  titlePanel("Scatter Plot with colour"),
 
-#### MAIN PAGE ####
-ui <- fluidPage(theme = shinytheme("paper"),
-                title = "General Insurance Class",
-                navbarPage(
-                  title = span(
-                    img(src = "logo_zurich.png", height = 30),
-                    "General Insurance Class"
-                  ),
-                  tabPanel("lesson3",
-                           tagList(
-                             fluidRow(div(
-                               style = "text-align:center", h3("This is second class about KPIs")
-                             )),
-                             navlistPanel(widths = c(2, 6),
-                                          tabPanel(
-                                            "Focus Group",
-                                            fluidRow(column(
-                                              width = 3,
-                                              offset = 9,
-                                              uiOutput("lesson3_KPI_multidim_select_axis_render")
-                                            )),
-                                            fluidRow(
-                                              column(width = 9,
-                                                     DT::dataTableOutput("lesson3_KPI_multidim_table")),
-                                              column(
-                                                width = 3,
-                                                plotOutput("lesson3_KPI_multidim_ratio_graph",
-                                                           width = 400),
-                                                plotOutput("lesson3_KPI_multidim_UWR_graph",
-                                                           width = 400)
-                                              )
-                                            )
-                                          ))
-                           ))
-                ))
+  sidebarLayout(      
+    sidebarPanel(
+      selectInput("select", "Colouring Var",
+                  choices=colnames(dt_KPI)[1:5]),
+      hr()
+  ),
+  mainPanel(
+    plotOutput(outputId = "plot")  
+  )
+)
+)
