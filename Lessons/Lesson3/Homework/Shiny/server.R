610eed01297df1151f3397d442c3155592a0fea4
Tekvicka <47779415+Tekvicka@users.noreply.github.com>
2019-03-11 20:55:41 +0100

    Update server.R

--- Lessons/Lesson3/Homework/Shiny/server.R
+++ Lessons/Lesson3/Homework/Shiny/server.R
@@ -1,89 +1,26 @@
 
-#### MAIN ####
-server <- function(input, output, session){
-  
-  
-  # Data for lesson 2 load
-  data_lesson3_react <- reactive({
-    read.csv("data/lesson3_KPI.csv")
+library(ggplot2)
+library(dplyr)
+library(shiny)
+
+dt_KPI <- read.csv("./Data/lesson2_KPI.csv") %>% filter_all(all_vars(!is.na(.)))
+
+server <- function(input, output){
+  output$plot <- renderPlot({
+    ggplot(data = dt_KPI, mapping = aes_string(x = "Premium", y = "Expenses", colour = input$select)) +
+      geom_point() +
+      geom_smooth()
   })
-  
-  # Definition of basic KPIs
-  data_lesson3_KPI_prep_react <- reactive({
-    dt <- data_lesson3_react()
-    # 
-    # prepared <- 
-    #   dt %>% # copy all data manipulation we have done in data preparation for KPIs definition
-    #   #
-    #   #
-    #   #
-    #   #
-    #   
-    #   prepared
-  })
-  
-  #### Sub-Tab Multi Dim (Focus Group) ####
-  # dynamically generate filter widgets if columns are categorical (character or factor)
-  source(file.path("Support", "dynamic_filter.R"),
-         local = TRUE)$value
-  
-  ######################################
-  output$lesson3_KPI_multidim_table <- renderDataTable({
-    
-    # dt_prep <- data_lesson3_KPI_multidim_prep_filter_react()
-    # 
-    # dt_prep <- 
-    #   dt_prep %>%
-    #   group_by_at(vars(input$lesson3_kpi_multidim_select_axis)) %>% 
-    #   # summarize(LR = , 
-    #   #           ER = ,
-    #   #           CoR = ,
-    #   #           Premium = ,
-    #   #           UWR = 
-    #   # )
-    #   
-    #   
-    #   DT::datatable(dt_prep,
-    #                 rownames = FALSE,
-    #                 class = "hover") %>% 
-    #   DT::formatPercentage(c("LR", "ER", "CoR")) %>% 
-    #   DT::formatCurrency(c("UWR", "Premium"), digits = 0) 
-  })
-  
-  output$lesson3_KPI_multidim_ratio_graph <- renderPlot({
-    
-    # data_lesson3_KPI_multidim_prep_filter_react() %>% 
-    #   group_by_at(vars(input$lesson3_kpi_multidim_select_axis)) %>% 
-    #   # summarize(LR = , 
-    #   #           ER = ,
-    #   #           CoR = ,
-    #   #           Premium = ,
-    #   #           UWR = 
-    #   # ) %>% 
-    #   ggplot() +
-    #   geom_col(
-    #     #
-    #     #
-    #     #
-    #     #
-    #     #
-    #   ) +
-    #   geom_hline(yintercept = 1) +
-    #   ylab("Ratios") +
-    #   scale_y_continuous(labels = scales::percent) +
-    #   theme_bw() +
-    #   coord_flip()
-    
-  })
-  
-  output$lesson3_KPI_multidim_UWR_graph <- renderPlot({
-    # # Homework
-    # data_lesson3_KPI_multidim_prep_filter_react() %>%
-      
-      
-  })
-  
-  
-  
-  
 }
+
+
+###########################################################################################################
+
+# Why categories behave differently? 
+##### kategoria - Business:
+# cestovne poistenie (Travel) je velmi rizikove preto nadobuda vysoke hodnoty Expenses (aj Premium)
+# najstrmsi sklon ma krivka pre poistenie domacnosti (Housing), co moze byt sposobene tym, ze toto poistenie je jedno z najkomplexnejších druhov poistení
+##### kategoria - Region:
+# z daneho grafu by sme mohli vyvodit ze oblast Alandia je pravdepodobnejsie najvacsia (najrizikovejsia), kedze ma najvyssie Expenses 
+##### kategoria - Unit:
+# vidime, ze Unit7 pravdepodobne obsahuje najrizikovejsie spolocnosti, kedze ma najvyssie Expenses
